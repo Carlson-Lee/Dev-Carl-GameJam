9acfbb11f5deed42f456f276e4a615ca19a5eb21
Carlson-poly <carlsonzhong@gmail.com>
2024-03-26 14:52:31 +1300

    working on parallax effect scene

--- BrainGame_GameJam1/Assets/Scripts/DialogueTrigger.cs
+++ BrainGame_GameJam1/Assets/Scripts/DialogueTrigger.cs
@@ -9,7 +9,7 @@ public class DialogueTrigger : MonoBehaviour
     void Update()
     {
         // Check for mouse click or space bar input
-        if (!dialogueManager.isTyping && Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        if (!dialogueManager.isTyping && Input.GetMouseButtonDown(0) || !dialogueManager.isTyping && Input.GetKeyDown(KeyCode.Space))
         {
             // Trigger the next dialogue
             dialogueManager.NextDialogue();
