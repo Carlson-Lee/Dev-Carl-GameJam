ec4a21a0b3c606d1c698c373435ebacf573ed5e1
Carlson-poly <carlsonzhong@gmail.com>
2024-04-04 13:08:39 +1300

    basic parallax movement working

--- BrainGame_GameJam1/Assets/Scripts/ParallaxMovement.cs
+++ BrainGame_GameJam1/Assets/Scripts/ParallaxMovement.cs
@@ -4,36 +4,21 @@ using UnityEngine;
 
 public class ParallaxMovement : MonoBehaviour
 {
-    private float _startingPos; //This is starting position of the sprites.
-    private float _lengthOfSprite;    //This is the length of the sprites.
-    public float AmountOfParallax;  //This is amount of parallax scroll. 
-    public Camera MainCamera;   //Reference of the camera.
+    Material mat;
+    float distance;
+
+    [Range(0f,0.5f)]
+    public float speed=0.2f;
 
         private void Start()
         {
-            //Getting the starting X position of sprite.
-            _startingPos = transform.position.x;    
-            //Getting the length of the sprites.
-            _lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+            mat = GetComponent<Renderer>().material;
             
         }
 
         private void Update()
         {
-            Vector3 Position = MainCamera.transform.position;
-            float Temp = Position.x * (1 - AmountOfParallax);
-            float Distance = Position.x * AmountOfParallax;
-
-            Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
-
-            transform.position = NewPosition;
-            if (Temp > _startingPos + (_lengthOfSprite / 2))
-            {
-                _startingPos += _lengthOfSprite;
-            }
-            else if (Temp < _startingPos - (_lengthOfSprite / 2))
-            {
-                _startingPos -= _lengthOfSprite;
-            }
+            distance += Time.deltaTime*speed;
+            mat.SetTextureOffset("_MainTex", Vector2.right * distance);
         }
 }
