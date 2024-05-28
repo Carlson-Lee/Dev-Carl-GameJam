28b203da3e65d22c1146930ad3107e74d5d3d4af
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-05-28 18:06:55 +1200

    ADD: Player movement left/right only with new IA and controller; Mostly just testing currently

---
+++ BrainGame_GameJam1/Assets/Scripts/GameJam2/SideScrollerPlayerController.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SideScrollerPlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Vector2 movementInput;
+    [SerializeField] private bool isGrounded = false;
+
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private PlayerInput playerInput;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private PlayerStates currentState; //Check for current animation needed
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // Switch to SideScroller action map
+        playerInput.SwitchCurrentActionMap("SideScroller");
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateAnimationState();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movementInput.x * movementSpeed, rb.velocity.y);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void UpdateMovement()
+    {
+        if (movementInput.x != 0)
+        {
+            sr.flipX = movementInput.x < 0;
+        }
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (movementInput.x != 0)  //If an input is being pressed
+        {
+            CurrentState = PlayerStates.WALK;
+            animator.SetFloat("xMove", Mathf.Abs(movementInput.x));
+            animator.SetBool("isGrounded", isGrounded);
+            animator.SetFloat("yVelocity", rb.velocity.y);
+        }
+        else
+        {
+            CurrentState = PlayerStates.IDLE;
+        }
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
+    private void OnJump(InputValue jumpValue)
+    {
+        if (jumpValue.isPressed && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDisable()
+    {
+        // Revert to the Player action map when this controller is disabled
+        playerInput.SwitchCurrentActionMap("Player");
+    }
+
+    /// <summary>
+    /// Possible animation states for the player character in the animator.
+    /// </summary>
+    public enum PlayerStates
+    { IDLE, WALK, ATTACK, DIE }
+
+    /// <summary>
+    /// Handles the switching and setting of the current animation state.
+    /// </summary>
+    PlayerStates CurrentState
+    {
+        set
+        {
+            currentState = value;
+
+            switch (currentState)
+            {
+                case PlayerStates.IDLE:
+                    animator.Play("Idle");
+                    break;
+                case PlayerStates.WALK:
+                    animator.Play("Walk");
+                    break;
+                case PlayerStates.ATTACK:
+                    animator.Play("Attack");
+                    break;
+                case PlayerStates.DIE:
+                    animator.Play("Die");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
