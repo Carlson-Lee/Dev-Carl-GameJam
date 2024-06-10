b380ac7a87b3d6d01d19898582ca7530de8368b7
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-06-10 17:33:44 +1200

    ADD: Second checkpoint and lights when checkpoint is activated

--- BrainGame_GameJam1/Assets/Scripts/GameJam2/SetCheckpointSpawn.cs
+++ BrainGame_GameJam1/Assets/Scripts/GameJam2/SetCheckpointSpawn.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class SetCheckpointSpawn : MonoBehaviour
 {
-    [SerializeField] private ResetPlayerPosition playerReset; 
-
+    [SerializeField] private ResetPlayerPosition playerReset;
+    [SerializeField] private GameObject light;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             playerReset.respawnPosition = this.transform.position;
+            light.SetActive(true);
         }
     }
 
