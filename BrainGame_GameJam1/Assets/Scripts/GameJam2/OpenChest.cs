dbc0369f155fb387486b37bd89cec6270cf0c837
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-06-10 20:52:17 +1200

    ADD: System + UI for picking up collectibles

--- BrainGame_GameJam1/Assets/Scripts/GameJam2/OpenChest.cs
+++ BrainGame_GameJam1/Assets/Scripts/GameJam2/OpenChest.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class OpenChest : MonoBehaviour
 {
     [SerializeField] private TilemapRenderer closedChest;
     [SerializeField] private TilemapRenderer openChest;
+
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private SpriteRenderer collectible;
+    [SerializeField] private RawImage UI_display;
+    private bool playerInTrigger = false;
 
     void Start()
     {
@@ -15,11 +22,37 @@ public class OpenChest : MonoBehaviour
         openChest.enabled = false;
     }
 
+    private void Update()
+    {
+        if (playerInTrigger && Input.GetKey(KeyCode.X))
+        {
+            PickupItem();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Used to only pickup one item at a time
+        if (collision.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+        
         closedChest.enabled = false;
         openChest.enabled = true;
+        text.gameObject.SetActive(true);
+        collectible.enabled = true;
 
         //Add other behaviour here later for UI collectibles
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInTrigger = false;
+    }
+    private void PickupItem()
+    {
+        this.collectible.enabled = false;
+        this.UI_display.enabled = true;
     }
 }
