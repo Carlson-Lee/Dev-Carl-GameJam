016c0f055524bb905852f00cd8cab2ac5342098c
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-06-10 18:02:03 +1200

    FIX: Checkpoints can now only be triggered once. Removes accidentally setting checkpoint as an old one

--- BrainGame_GameJam1/Assets/Scripts/GameJam2/SetCheckpointSpawn.cs
+++ BrainGame_GameJam1/Assets/Scripts/GameJam2/SetCheckpointSpawn.cs
@@ -6,13 +6,21 @@ public class SetCheckpointSpawn : MonoBehaviour
 {
     [SerializeField] private ResetPlayerPosition playerReset;
     [SerializeField] private GameObject light;
+
+    private BoxCollider2D collider;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             playerReset.respawnPosition = this.transform.position;
             light.SetActive(true);
+            
         }
+
+        //Used to disable old checkpoints being triggered again
+        collider = this.GetComponent<BoxCollider2D>();
+        collider.enabled = false;
     }
 
 }
